957be37f4de58a2e5f69087207507c61ccbf5da3
Tiffany Tu <tiffanytu2024@gmail.com>
2023-06-29 00:14:12 -0700

    Initial Commit

---
+++ ResourcesTest/ResourcesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResourcesTestApp.swift
+//  ResourcesTest
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ResourcesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
