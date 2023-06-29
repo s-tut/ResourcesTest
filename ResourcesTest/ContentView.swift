3855e428e26f0a6b348e0a741f2b7ecf604b90f9
Tiffany Tu <tiffanytu2024@gmail.com>
2023-06-29 02:12:31 -0700

    more!

--- ResourcesTest/ContentView.swift
+++ ResourcesTest/ContentView.swift
@@ -13,52 +13,72 @@ struct ContentView: View
     {
         ZStack
         {
-            Color("#9AC5F4")
+            
+            Color(.systemCyan)
                 .ignoresSafeArea()
             
-            Text("Resources")
-                .font(.title)
+            VStack {
+                HStack
+                {
+                    Text("Resources")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding()
+                }
+                Spacer()
+            }
             
             VStack(spacing:30)
             {
                 Link(destination: URL(string: "https://www.world-of-lucid-dreaming.com/30-common-dream-symbols.html")!, label:
                         {
-                    Text("Common Dream Symbols")
+                    Text("Dream Symbols")
                         .bold()
-                        .frame(width: 280, height: 50)
+                        .frame(width: 280, height: 70)
                         .foregroundColor(.white)
                         .background(Color.black)
-                        .cornerRadius(12)
+                        .cornerRadius(15)
                 })
                 
                 Link(destination: URL(string: "https://health.clevelandclinic.org/dreams-and-dreaming/")!, label:
                         {
                     Text("What Dreams Mean")
                         .bold()
-                        .frame(width: 280, height: 50)
+                        .frame(width: 280, height: 70)
                         .foregroundColor(.white)
                         .background(Color.black)
-                        .cornerRadius(12)
+                        .cornerRadius(15)
                 })
                 
                 Link(destination: URL(string: "https://thesleepdoctor.com/dreams/mental-health-and-dreams/")!, label:
                         {
                     Text("Dreams and Mental Health")
                         .bold()
-                        .frame(width: 280, height: 50)
+                        .frame(width: 280, height: 70)
                         .foregroundColor(.white)
                         .background(Color.black)
-                        .cornerRadius(12)
+                        .cornerRadius(15)
                 })
                 
                 Link(destination: URL(string: "https://www.healthline.com/health/healthy-sleep/how-to-lucid-dream")!, label:
                         {
                     Text("Lucid Dreaming")
                         .bold()
-                        .frame(width: 280, height: 50)
+                        .frame(width: 280, height: 70)
                         .foregroundColor(.white)
                         .background(Color.black)
-                        .cornerRadius(12)
+                        .cornerRadius(15)
+                })
+                
+                Link(destination: URL(string: "https://www.insider.com/dream-expert-shares-5-most-common-dreams-and-their-meaning-2023-4")!, label:
+                        {
+                    Text("Common Dreams")
+                        .bold()
+                        .frame(width: 280, height: 70)
+                        .foregroundColor(.white)
+                        .background(Color.black)
+                        .cornerRadius(15)
                 })
             }
             
@@ -67,6 +87,7 @@ struct ContentView: View
                 .cornerRadius(15)
                 .shadow(radius:15)
             .padding()
+            
         }
     }
 }
