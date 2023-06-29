37dbea338d6f98223c90c4090b23b40514aced7a
Tiffany Tu <tiffanytu2024@gmail.com>
2023-06-29 10:58:46 -0700

    more changes

--- ResourcesTest/ContentView.swift
+++ ResourcesTest/ContentView.swift
@@ -13,17 +13,14 @@ struct ContentView: View
     {
         ZStack
         {
-            
-            Color((red : 154/255, green: 197/255, blue:  244/255) as! CGColor)
-                .ignoresSafeArea()
-            
             VStack {
                 HStack
                 {
                     Text("Resources")
                         .font(.largeTitle)
                         .fontWeight(.bold)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .foregroundColor(Color(red:255/255, green: 238/255, blue: 187/255))
                         .padding()
                 }
                 Spacer()
@@ -33,62 +30,67 @@ struct ContentView: View
             {
                 Link(destination: URL(string: "https://www.world-of-lucid-dreaming.com/30-common-dream-symbols.html")!, label:
                         {
-                    Text("Dream Symbols")
+                    Text("Dream Symbols 🌙")
                         .bold()
                         .frame(width: 280, height: 70)
-                        .foregroundColor(.white)
-                        .background(Color.black)
+                        .foregroundColor(.black)
+                        .background(Color(red:153/255, green: 219/255, blue: 245/255))
                         .cornerRadius(15)
+                        .shadow(radius:15)
                 })
                 
                 Link(destination: URL(string: "https://health.clevelandclinic.org/dreams-and-dreaming/")!, label:
                         {
-                    Text("What Dreams Mean")
+                    Text("What Dreams Mean ☁️")
                         .bold()
                         .frame(width: 280, height: 70)
-                        .foregroundColor(.white)
-                        .background(Color.black)
+                        .foregroundColor(.black)
+                        .background(Color(red:153/255, green: 219/255, blue: 245/255))
                         .cornerRadius(15)
+                        .shadow(radius:15)
                 })
                 
                 Link(destination: URL(string: "https://thesleepdoctor.com/dreams/mental-health-and-dreams/")!, label:
                         {
-                    Text("Dreams and Mental Health")
+                    Text("Dreams and Mental Health 🧘")
                         .bold()
                         .frame(width: 280, height: 70)
-                        .foregroundColor(.white)
-                        .background(Color.black)
+                        .foregroundColor(.black)
+                        .background(Color(red:153/255, green: 219/255, blue: 245/255))
                         .cornerRadius(15)
+                        .shadow(radius:15)
                 })
                 
                 Link(destination: URL(string: "https://www.healthline.com/health/healthy-sleep/how-to-lucid-dream")!, label:
                         {
-                    Text("Lucid Dreaming")
+                    Text("Lucid Dreaming 🛌")
                         .bold()
                         .frame(width: 280, height: 70)
-                        .foregroundColor(.white)
-                        .background(Color.black)
+                        .foregroundColor(.black)
+                        .background(Color(red:153/255, green: 219/255, blue: 245/255))
                         .cornerRadius(15)
+                        .shadow(radius:15)
                 })
                 
                 Link(destination: URL(string: "https://www.insider.com/dream-expert-shares-5-most-common-dreams-and-their-meaning-2023-4")!, label:
                         {
-                    Text("Common Dreams")
+                    Text("Common Dreams 💭")
                         .bold()
                         .frame(width: 280, height: 70)
-                        .foregroundColor(.white)
-                        .background(Color.black)
+                        .foregroundColor(.black)
+                        .background(Color(red:153/255, green: 219/255, blue: 245/255))
                         .cornerRadius(15)
+                        .shadow(radius:15)
                 })
             }
             
             .padding()
-            .background(Rectangle().foregroundColor(.white))
+            .background(Color(red:255/255, green: 238/255, blue: 187/255))
                 .cornerRadius(15)
-                .shadow(radius:15)
             .padding()
             
         }
+        .background(Color(red:154/255, green: 197/255, blue: 244/255))
     }
 }
 
