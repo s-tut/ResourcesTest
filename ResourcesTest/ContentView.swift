3c10e9c550794a0fb3c723b9062c7a0537055e4e
Tiffany Tu <tiffanytu2024@gmail.com>
2023-06-29 01:17:56 -0700

    stuff

--- ResourcesTest/ContentView.swift
+++ ResourcesTest/ContentView.swift
@@ -7,20 +7,61 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ContentView: View
+{
+    var body: some View
+    {
+        VStack(spacing:30)
+        {
+            Link(destination: URL(string: "https://www.world-of-lucid-dreaming.com/30-common-dream-symbols.html")!, label:
+            {
+                Text("Common Dream Symbols")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(12)
+            })
+            
+            Link(destination: URL(string: "https://health.clevelandclinic.org/dreams-and-dreaming/")!, label:
+            {
+                Text("What Dreams Mean")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(12)
+            })
+            
+            Link(destination: URL(string: "https://thesleepdoctor.com/dreams/mental-health-and-dreams/")!, label:
+            {
+                Text("Dreams and Mental Health")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(12)
+            })
+            
+            Link(destination: URL(string: "https://www.healthline.com/health/healthy-sleep/how-to-lucid-dream")!, label:
+            {
+                Text("Lucid Dreaming")
+                    .bold()
+                    .frame(width: 280, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(12)
+            })
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         ContentView()
     }
 }
+
+//Link("Apple", destination: URL(string:"https://www.apple.com")!)
